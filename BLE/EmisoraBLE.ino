436a572461b7f7d241c7ae441347227df2cb7143
Víctor White <36444816+vicblab1@users.noreply.github.com>
2019-10-06 17:48:40 +0200

    Update EmisoraBLE.ino

--- BLE/EmisoraBLE.ino
+++ BLE/EmisoraBLE.ino
@@ -3,19 +3,28 @@
 class EmisoraBLE{
   private:
 
-  BLEUart bleuart;
-   uint8_t beaconUUID[16]={NULL};/* = {
+  
+  BLEBeacon elBeacon;
+   uint8_t beaconUUID[16] ={
      'E', 'P', 'S', 'G', '-', 'G', 'T', 'I',
      '-', 'P', 'R', 'O', 'Y', '-', '3', 'A'
-     };*/
+     };
+     char nombre[16]="GTI-3A-Equipo_5";//{
+      //'G','T','I','-','3','A','-','E','q','u','i','p','o','_','0','5'
+      //};
   
   
   public:
 
-  EmisoraBLE(uint8_t *(beaconUUID_), char nombre[24]){
-      // Initialise the Bluefruit module
-   //
-   Serial.println("Initialise the Bluefruit nRF52 module");
+  EmisoraBLE(){
+    
+    }
+
+   void inicializar(){
+
+   elBeacon.setUuid( beaconUUID);
+    
+     Serial.println("Initialise the Bluefruit nRF52 module");
    Bluefruit.begin();
 
    //
@@ -27,12 +36,15 @@ class EmisoraBLE{
    Bluefruit.ScanResponse.addName();
     }
 
-    void anunciarX(uint8_t elMajor, uint8_t elMinor){
+   void anunciarX(uint8_t temperatura, uint8_t humedad, uint8_t medida){
 
-       BLEBeacon elBeacon( beaconUUID, 12, 34, 73 );
+  uint16_t elMajor=((uint16_t)temperatura << 8) | humedad;
+  uint16_t elMinor=(uint16_t)medida;
+  
+      
       Bluefruit.Advertising.stop(); // ya lo enchufo luego
       elBeacon.setMajorMinor(elMajor, elMinor);
       Bluefruit.Advertising.setBeacon(elBeacon);
-    }
+    };
   
   };
