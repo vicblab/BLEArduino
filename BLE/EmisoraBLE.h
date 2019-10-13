af1254b953f1337b4d522b0fed5b53202f575919
Víctor White <36444816+vicblab1@users.noreply.github.com>
2019-10-13 19:13:22 +0200

    Major y minor anunciados correctamente

--- BLE/EmisoraBLE.h
+++ BLE/EmisoraBLE.h
@@ -33,30 +33,42 @@ class EmisoraBLE{
 
    Bluefruit.setName(nombre);
    Bluefruit.ScanResponse.addName();
-   Bluefruit.Advertising.start();
+   //Bluefruit.Advertising.start();
    //Bluefruit.Advertising.setBeacon(elBeacon);
    anunciarCO(0,0,0);
     }
 
    void anunciarCO(int temperatura, int humedad, int medida){
 
-  BLEBeacon elBeacon( beaconUUID, 12, 34, 73 );
-  temperatura=0;
-  humedad=0;
-  medida=0;
-  uint16_t elMajor=((uint16_t)temperatura << 8) | (uint8_t)humedad;
+ // temperatura=0;
+  //humedad=0;
+  //medida=0;
+
+  uint16_t elMajor= (uint16_t)100*temperatura+humedad;//((uint16_t)temperatura << 8) | (uint8_t)humedad;
   uint16_t elMinor=(uint16_t)medida;
-  
+  //elMajor=(uint16_t)char(elMajor);
+   //elMinor=(uint16_t)char(elMinor);
       Serial.print("\n Major: ");
       Serial.print(elMajor);
       Serial.print("\n Minor: ");
       Serial.print(elMinor);
-    //  Bluefruit.Advertising.stop(); // ya lo enchufo luego
+      
+  BLEBeacon elBeacon( beaconUUID, elMajor, elMinor, 73 );
+      Bluefruit.Advertising.stop(); // ya lo enchufo luego
          Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
    Bluefruit.Advertising.addTxPower();
       elBeacon.setMajorMinor(elMajor, elMinor);
       Bluefruit.Advertising.setBeacon(elBeacon);
-      Serial.println(nombre);
+        Bluefruit.Advertising.start();
+        
+        //descomentar para imprimir todos los datos del advertising uno a uno:
+      /*for(int i=1; i<62; i++){
+        Serial.print(i);
+        Serial.print(": ");
+          Serial.print(Bluefruit.Advertising.getData()[i-1]);
+          Serial.print("\n");
+        }*/
+    
     };
   
   };
